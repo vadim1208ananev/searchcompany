2b89215181f26987bb4db0e20733d21046e58be5
Vadim <vadim08ananev@gmail.com>
2022-10-17 14:17:55 +0300

    reserv

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,23 @@
   <div class="container">
      <app-top></app-top>
      <div class="block-form">
-
+         <div class="block-notify">
+             <div class="top-notify"></div>
+             <div class="bottom-notify"></div>
+         </div>
+         <div class="own-form">
+             <input
+                     type="text"
+                     v-model="finder"
+                     placeholder="Enter your desired company name"
+                     @keydown.enter="SendMessage"
+             >
+             <button type="button">Ckeck Now</button>
+         </div>
      </div>
     <app-bottom></app-bottom>
 
   </div>
-
 
 </template>
 
